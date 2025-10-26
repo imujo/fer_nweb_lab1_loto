e5d03c10756f117df4ecdda470e2e67c023f1535
imujo <ivo.mujo.3@gmail.com>
2025-10-26 10:44:49 +0100

    remove nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
   plugins: [
     netlify(),
-    nitroV2Plugin(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
